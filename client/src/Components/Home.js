6ac455e01acc795cb41831605c7b880ebcefb159
Jeremy Parker <jeremyeparker@yahoo.com>
2020-09-12 22:23:01 -0400

    add header and buttons linked to login/registeration pages

---
+++ client/src/Components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Button from './Button'
+
+export default function home() {
+    return (
+        <div>
+            <h1>Homie Page</h1>
+            <Button 
+            text="Register"
+            onClick={() =>{
+                window.location = window.location.origin + "/register"
+            }}
+            ></Button>
+            <Button 
+            text="Login"
+            onClick={() =>{
+                window.location = window.location.origin + "/login"
+            }}
+            ></Button>
+        </div>
+    )
+}
