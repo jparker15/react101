b7535d1e3235a24d4b0983bb56ffd8800814363a
Jeremy Parker <jeremyeparker@yahoo.com>
2020-09-14 22:51:30 -0400

    add register form, change window.locations,

--- client/src/Components/Register.js
+++ client/src/Components/Register.js
@@ -1,21 +1,32 @@
 import React from 'react'
 import Button from './Button'
+import Form from './Form'
+
+import {registInputs} from "../utils/userInputs"
+import {registReq} from "../utils/userRequests"
 
 export default function register() {
     return (
         <div>
             <h1>Vehicle Registration</h1>
+
+            <Form
+                id="registerForm"
+                title="Register"
+                inputs={registInputs}
+                submitFunc={registReq}
+            />
             <Button
             text="Return to Home"
             onClick={()=>{
-                window.location=window.location.origin
+                window.location="/"
             }}
             
             ></Button>
             <Button
-            text="Login"
+            text="Already a User? Login"
             onClick={()=>{
-                window.location=window.location.origin + "/login"
+                window.location="/login"
             }}
             
             ></Button>
