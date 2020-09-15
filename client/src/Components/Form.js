dd9feea8245801ba6b63a597ab3252f33df97a68
Jeremy Parker <jeremyeparker@yahoo.com>
2020-09-15 19:06:13 -0400

    form comp uses map to compile inputs and dynamically change them via inProps, added submit Button

---
+++ client/src/Components/Form.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Button from './Button'
+import Input from './Input'
+
+export default function Form(props) {//inputs=Array, title=String, submitFunc= function
+
+    const btnOnClick = () =>{
+        props.submitFunc(document.getElementById(props.id));
+    }
+
+    return (
+        <div>
+            <h2>
+                {props.title}
+            </h2>
+            <form id= {props.id}>
+                {
+                    Array.isArray(props.inputs)
+                    ?
+                    props.inputs.map( inProps =>{
+                        return(
+                            <Input
+                                name={inProps.name}
+                                placeholder={inProps.placeholder}
+                                type={inProps.type}
+                                style={inProps.style}
+                                id={inProps.id}
+                                onChange={inProps.onChange}
+                            />
+                        )
+                    }) : "Dev Warning: No Inputs: Check Form Code"
+                }
+            </form>
+            <Button
+                text="submit"
+                onClick={btnOnClick}
+            />
+        </div>    
+    )
+}
