1aaf5f2e5e3101efc2a7e942af37749ff5daac8a
Jeremy Parker <jeremyeparker@yahoo.com>
2020-09-14 22:42:05 -0400

    allows custom inputs for differnt forms

---
+++ client/src/utils/userInputs.js
@@ -0,0 +1,17 @@
+module.exports ={
+
+  loginInputs:[
+    {name: 'username', placeholder: 'Enter Username', type: 'text'},
+    {name: 'email', placeholder: 'Enter Email', type: 'text'},
+    {name: 'password', placeholder: 'Enter Password', type: 'password'}
+  ],
+  registInputs: [
+    
+      {name: 'username', placeholder: 'Enter Username', type: 'text'},
+      {name: 'email', placeholder: 'Enter Email', type: 'text'},
+      {name: 'password', placeholder: 'Enter Password', type: 'password'},
+      {name: 'password2', placeholder: 'Confirm Password', type: 'password'}
+    
+  ]
+
+} 
