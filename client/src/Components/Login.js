087465ee32178721277cbe64748ab8e7f2f96b2c
Jeremy Parker <jeremyeparker@yahoo.com>
2020-09-14 22:49:32 -0400

    add loginForm

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import Button from './Button'
+import Form from "./Form"
+
+import {loginReq} from "../utils/userRequests"
+import {loginInputs} from "../utils/userInputs"
 
 export default function login() {
     return (
         <div>
             <h1>Login</h1>
+
+            <Form
+                id="loginForm"
+                inputs={loginInputs}
+                title="User Login"
+                submitFunc={loginReq}
+
+            />
+
+            <br/>
             <Button
             text="Return to Home Page"
             onClick={()=>{
