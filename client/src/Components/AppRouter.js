24a1bcdb2e3cb9aea5dd14109bcae63b14838685
Jeremy Parker <jeremyeparker@yahoo.com>
2020-09-14 22:33:40 -0400

    remove return Home button

--- client/src/Components/AppRouter.js
+++ client/src/Components/AppRouter.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Route,Switch} from "react-router-dom";
-import Button from './Button';
 import Home from './Home';
 import Login from './Login';
 import Register from './Register';
@@ -8,13 +7,6 @@ import Register from './Register';
 export default function AppRouter() {
     return (
         <div>
-            <nav></nav>
-            <Button
-            text="Back to Home"
-            onClick = {() =>{
-                window.location = window.location.origin
-            }}
-            />
             <Switch>
                 <Route path="/" exact>
                     <Home/>
