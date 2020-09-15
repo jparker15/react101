78c8e404b9e8c082bac405821294507690404d51
Jeremy Parker <jeremyeparker@yahoo.com>
2020-09-14 22:48:57 -0400

    returns an input with props set to props. and defaultStyle

---
+++ client/src/Components/Input.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Input(props) {
+
+    return (
+        <input
+            //ph, type, onchange, name, id
+            id = {props.id}
+            name= {props.name}
+            type={props.type}
+            placeholder={props.placeholder}
+            onChange={props.onChange}
+            style={{...defaultStyle, ...props.style}}
+        
+        />
+    )
+}
+
+const defaultStyle ={
+    color: "grey",
+    backgroundColor: "lightblue",
+    border: "1px solid white",
+    padding:  "20px"
+}
