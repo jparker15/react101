a98c72abadcb00a8cef123aeb9bb3800b25e8a24
Jeremy Parker <jeremyeparker@yahoo.com>
2020-09-15 23:56:55 -0400

    update loginInputs to include credential instead of username/email

--- client/src/utils/userInputs.js
+++ client/src/utils/userInputs.js
@@ -1,8 +1,7 @@
 module.exports ={
 
   loginInputs:[
-    {name: 'username', placeholder: 'Enter Username', type: 'text'},
-    {name: 'email', placeholder: 'Enter Email', type: 'text'},
+    {name: 'credential', placeholder: 'Enter Username or Email', type: 'text'},
     {name: 'password', placeholder: 'Enter Password', type: 'password'}
   ],
   registInputs: [
