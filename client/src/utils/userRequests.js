d96069503405a9c1b9d337e9230ed62b1612bd29
Jeremy Parker <jeremyeparker@yahoo.com>
2020-09-15 20:05:39 -0400

    complete register request, add axios.post

--- client/src/utils/userRequests.js
+++ client/src/utils/userRequests.js
@@ -23,7 +23,7 @@ module.exports = {
             },
             method: "PUT",
             data: JSON.stringify(reqBody)
-        }
+        };
 
         axios.put(loginURL, reqData)
         .then( res =>{
@@ -34,9 +34,45 @@ module.exports = {
                 console.log(err);
             }
         })
+        console.log(reqBody)
     }
     ,
     registReq: (form) =>{
         console.log("test log of form",form);
+
+        const reqBody = {}
+
+        for (const input of form) {
+            const val = input.value;
+
+            if(val !== ""){
+                reqBody[input.name] = val
+            }
+        }
+
+        const registURL = `${baseURL}/user/register`;
+
+        const reqData = {
+            headers:{
+                Accept:"application/json",
+                "Content-Type":"applicaiton/json",
+                "Access-Control-Allow-Origin":"*"
+            },
+            method:"POST",
+            data: JSON.stringify(reqBody)
+        };
+
+        axios.post(registURL, reqData)
+        .then(res =>{
+            console.log(res)
+        })
+        .catch( err=>{
+            if(err){
+                console.log(err);
+            }
+        })
+        console.log(reqBody);
+
+
     }
 }
