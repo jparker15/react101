a4d1669eeef40106a061cb89c883d67cf90fe3ae
Jeremy Parker <jeremyeparker@yahoo.com>
2020-09-15 20:03:52 -0400

    rename app.js -> server.js

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ app = express();
 const 
 userRouter = require('./api/routes/userRouter');
 //MIDDLEWARES
+const morgan = require("morgan"),
+cors = require("cors");
 
 //ENV CONTANTS
 const 
-PORT = process.env.PORT || 3000, //Port number for server to listen on, defined in enviorment file
+PORT = process.env.PORT || 4000, //Port number for server to listen on, defined in enviorment file
 URI = process.env.MONGO; // URI that gives read/write access to database, this need to be stored in an env so that the public can not read/write your database without going through your API     
 
 //MongoDB Utility Function Used To Connect To DB
@@ -23,6 +25,10 @@ const mongoConnect = require('./database/mongo-connect');
    This is because typically the specific routes will rely on one or more of the middlewares that run on all routes.
 */
 
+app.use(cors())
+app.use(express.json())
+app.use(morgan("dev"))
+
 //homepage route
 app.use('/user', userRouter);
 
@@ -30,4 +36,6 @@ app.use('/user', userRouter);
 //Connect to Data-Base
 mongoConnect(URI)
 //Start server listening on port
-app.listen(PORT);
+app.listen(PORT, () =>{
+   console.log(`listening on port :${PORT}`);
+});
