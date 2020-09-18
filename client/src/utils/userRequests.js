c7121dc9df6592c9d5458e545285da8047770867
Jeremy Parker <jeremyeparker@yahoo.com>
2020-09-17 22:08:57 -0400

    add validation for missing input values

--- client/src/utils/userRequests.js
+++ client/src/utils/userRequests.js
@@ -5,6 +5,7 @@ const baseURL = "http://localhost:3333";
 module.exports = {
     loginReq: (form) =>{
         const reqBody = {};
+        let missingInput = [];
 
         for (const input of form){
             const val = input.value;
@@ -12,12 +13,22 @@ module.exports = {
             if(val !== ""){
                 reqBody[input.name] = val
             }
+        
+            else{
+                missingInput.push(input.name)
+            }
         }
         //front end validation needed:
         // all fildes are present
-
+        if(missingInput.length > 0){
+            const misMsg = missingInput.map(input =>{
+                return alert(`${input} is required`)
+            });
+            return alert(misMsg)
+            
+        }
         // lengths of inputs match schema
-
+        
         // 
 
         const loginURL = `${baseURL}/user/login`;
@@ -48,14 +59,23 @@ module.exports = {
     registReq: (form) =>{
         console.log("test log of form",form);
 
-        const reqBody = {}
+        const reqBody = {};
+        let missInput = [];
 
         for (const input of form) {
             const val = input.value;
 
             if(val !== ""){
                 reqBody[input.name] = val
+            }else{
+                missInput.push(input.name)
             }
+        }
+
+        if(missInput.length > 0){
+            const misMsg = missInput.map((input)=>{return alert(`${input} is required`)})
+
+            return alert(misMsg);
         }
 
         const registURL = `${baseURL}/user/register`;
