4c781478be11ccf11092202ab8b4da675a731c00
Jeremy Parker <jeremyeparker@yahoo.com>
2020-09-20 23:56:40 -0400

    import uselocalstorage add input

--- client/src/Components/AppRouter.js
+++ client/src/Components/AppRouter.js
@@ -3,13 +3,18 @@ import {Route,Switch} from "react-router-dom";
 import Home from './Home';
 import Login from './Login';
 import Register from './Register';
+import useLocalStorage from './useLocalStorage';
 
 export default function AppRouter() {
+
+    const [name,setName] = useLocalStorage("name", "")
+
     return (
         <div>
             <Switch>
                 <Route path="/" exact>
                     <Home/>
+                    <input/>
                 </Route>
 
                 <Route path="/login" exact>
