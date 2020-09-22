e2ef43c039a5994b751327af541625df73fcfacd
Jeremy Parker <jeremyeparker@yahoo.com>
2020-09-22 17:54:23 -0400

    example of a theme toggle button

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React /*,{useState,useEffect}*/ from 'react';
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./Components/AppRouter";
 
 function App() {
+
+  // const [theme, setTheme] = useState(true)
+  // //console.log(theme)
+  // //runs everytime it renders instead of once when it mounts
+  // useEffect(() =>{
+  //   console.log(theme)
+  // }, [theme]) 
+
   return (
     <BrowserRouter>
+    {/* <button
+    onClick={()=>{
+      setTheme(prevTheme =>{return !prevTheme} )
+    }}
+    >
+      Change Me
+    </button> */}
       <AppRouter/>
     </BrowserRouter>
   );
